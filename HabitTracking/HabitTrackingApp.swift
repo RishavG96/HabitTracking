eead86ce20b891a6420e189d9f1ff4a25551961f
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-17 11:18:04 +0530

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Rishav Gupta on 17/06/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
